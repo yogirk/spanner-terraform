cf458d5ca5b9b602270ddbde0e519298a363a7bd
Rohan Singh <67792436+rohan-searce@users.noreply.github.com>
2021-01-26 14:48:54 +0530

    removed validation from project_id

--- cloudrun/blueprint/variables.tf
+++ cloudrun/blueprint/variables.tf
@@ -3,10 +3,6 @@
 variable "gcp_project_id" {
   description = "Id of the GCP project"
   type        = string
-  validation {
-    condition     = can(regex("^[a-z]{2,7}-[a-z0-9]{3,18}$", var.gcp_project_id))
-    error_message = "Must be of this format: `org-project` and a max of 30 characters."
-  }
 }
 
 variable "region" {
