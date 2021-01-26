44f274e0a29d89facce4c0be54eea0b3b4780f62
Rohan Singh <67792436+rohan-searce@users.noreply.github.com>
2021-01-26 14:59:15 +0530

    updated blueprint for both frontend and backend.

--- cloudrun/blueprint/main.tf
+++ cloudrun/blueprint/main.tf
@@ -8,11 +8,20 @@ locals {
   suffix = format("%s-%s", "tf", random_string.launch_id.result)
 }
 
-module "cloud_run" {
+module "cloud_run_backend" {
   source               = "../modules/cloud_run"
   suffix               = local.suffix
   service_name         = "stock-app"
-  container_image_path = "gcr.io/<PROJECT_ID>/docker-image-path:<tag>"
+  container_image_path = "gcr.io/<PROJECT_ID>/backend-docker-image-path:<tag>"
+  region               = var.region
+  gcp_project_id       = var.gcp_project_id
+}
+  
+module "cloud_run_frontend" {
+  source               = "../modules/cloud_run"
+  suffix               = local.suffix
+  service_name         = "frontend"
+  container_image_path = "gcr.io/<PROJECT_ID>/frontend-docker-image-path:<tag>"
   region               = var.region
   gcp_project_id       = var.gcp_project_id
 }
