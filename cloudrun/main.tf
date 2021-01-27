7e3ef07f3cb775758354333124021c84580ab6e9
Rohan Singh <67792436+rohan-searce@users.noreply.github.com>
2021-01-27 12:15:19 +0530

    removed GCS remote backend dependency

--- cloudrun/main.tf
+++ cloudrun/main.tf
@@ -1,10 +1,3 @@
-terraform {
-  backend "gcs" {
-    bucket = "[gcp-terraform-bucket-name]"
-    prefix = "main"
-  }
-}
-
 module infra {
   source         = "./blueprint/"
   gcp_project_id = "[gcp-project-id]"
