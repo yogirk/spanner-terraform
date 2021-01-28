14a99e431ea0cd6eb7ebc758859da545fa606f77
Rohan Singh <67792436+rohan-searce@users.noreply.github.com>
2021-01-28 21:06:23 +0530

    remove project id validation

--- cloudrun/modules/cloud_run/variables.tf
+++ cloudrun/modules/cloud_run/variables.tf
@@ -28,10 +28,6 @@ variable container_image_path {
 variable "gcp_project_id" {
   description = "Id of the GCP project"
   type        = string
-  validation {
-    condition     = can(regex("^[a-z]{2,7}-[a-z0-9]{3,18}$", var.gcp_project_id))
-    error_message = "Must be of this format: `org-project` and a max of 30 characters."
-  }
 }
 
 variable container_port {
