5375c64c54c0003ebf59b9c283572dc492f95952
vignesh shan <78083959+vigneshsdev@users.noreply.github.com>
2021-01-28 19:37:29 +0530

    Update database.js
    connection pool configuration added

--- stockapp/stock-trade/app/config/database.js
+++ stockapp/stock-trade/app/config/database.js
@@ -8,9 +8,30 @@ const spanner = new Spanner({projectId: process.env.PROJECTID,});
 const instance = spanner.instance(process.env.INSTANCE);  
 
 // Environment variable assigned here
-const databaseId = process.env.DATABASE;  
+const databaseId = process.env.DATABASE;
 
 // Initialize database
-const database = instance.database(databaseId);
+const database = instance.database(databaseId,{
+    acquireTimeout: Infinity,
+    concurrency: Infinity,
+    fail: false,
+    idlesAfter: 10,
+    keepAlive: 30,
+    labels: {},
+    max: 500,
+    maxIdle: 1,
+    min: 100
+});
+
+database.createSession(function(err, session, apiResponse) {
+    if (err) {
+     console.log(err)
+    }
+});
+  
+database.createSession().then(function(data) {
+    const session = data[0];
+    const apiResponse = data[1];
+});
 
 module.exports = database;
